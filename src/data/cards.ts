6a2e7d18dd5bc66b5ae3e89f9e052aa65e38a3bb
Óscar Bustos <oscarbustosgarrido@gmail.com>
2025-10-07 15:30:57 +0200

    feat: reorganize cards into data module and update image assets

---
+++ src/data/cards.ts
@@ -0,0 +1,109 @@
+// Importar todas las imágenes
+import imagenRuta from '../assets/paseo-faedo.webp';
+import imageVisitantes from '../assets/visitantes.webp';
+import imageBruja from '../assets/bruja.webp';
+import imageDesman from '../assets/desman-iberico.webp';
+import imageBlog from '../assets/blog.webp';
+import imageGeologia from '../assets/geologia.webp';
+import imageMineria from '../assets/pozo-ibarra.webp'
+import imageFagus from '../assets/fagus.webp'
+import imageNutria from '../assets/nutria.webp'
+
+export interface Card {
+  id: string;
+  href: string;
+  title: string;
+  description?: string;
+  icon: string;
+  backgroundImage?: any;
+  titleTag?: 'h2' | 'h3' | 'h4';
+}
+
+export const allCards: Card[] = [
+  {
+    id: 'ruta',
+    href: '/ruta/faedo-de-cinera',
+    title: 'La Ruta',
+    description: 'Consejos para disfrutar de tu ruta',
+    icon: 'M128,24A104,104,0,1,0,232,128,104.11,104.11,0,0,0,128,24Zm0,192a88,88,0,1,1,88-88A88.1,88.1,0,0,1,128,216ZM172.42,72.84l-64,32a8.05,8.05,0,0,0-3.58,3.58l-32,64A8,8,0,0,0,80,184a8.1,8.1,0,0,0,3.58-.84l64-32a8.05,8.05,0,0,0,3.58-3.58l32-64a8,8,0,0,0-10.74-10.74ZM138,138,97.89,158.11,118,118l40.15-20.07Z',
+    backgroundImage: imagenRuta.src
+  },
+  {
+    id: 'historia-naturaleza',
+    href: '/historia-naturaleza',
+    title: 'Historia y Naturaleza',
+    description: 'Conoce de primera mano su entorno',
+    icon: 'M198.1,62.59a76,76,0,0,0-140.2,0A71.71,71.71,0,0,0,16,127.8C15.9,166,48,199,86.14,200A72.09,72.09,0,0,0,120,192.47V232a8,8,0,0,0,16,0V192.47A72.17,72.17,0,0,0,168,200l1.82,0C208,199,240.11,166,240,127.8A71.71,71.71,0,0,0,198.1,62.59ZM169.45,184a56.08,56.08,0,0,1-33.45-10v-41l43.58-21.78a8,8,0,1,0-7.16-14.32L136,115.06V88a8,8,0,0,0-16,0v51.06L83.58,120.84a8,8,0,1,0-7.16,14.32L120,156.94v17a56,56,0,0,1-33.45,10C56.9,183.23,31.92,157.52,32,127.84A55.77,55.77,0,0,1,67.11,76a8,8,0,0,0,4.53-4.67,60,60,0,0,1,112.72,0A8,8,0,0,0,188.89,76,55.79,55.79,0,0,1,224,127.84C224.08,157.52,199.1,183.23,169.45,184Z',
+    backgroundImage: imageDesman.src
+  },
+  {
+    id: 'guia-visitantes',
+    href: '/guia-visitantes',
+    title: 'Guía para visitantes',
+    description: 'Alojamientos, restaurantes y normas ambientales',
+    icon: 'M232,48H160a40,40,0,0,0-32,16A40,40,0,0,0,96,48H24a8,8,0,0,0-8,8V200a8,8,0,0,0,8,8H96a24,24,0,0,1,24,24,8,8,0,0,0,16,0,24,24,0,0,1,24-24h72a8,8,0,0,0,8-8V56A8,8,0,0,0,232,48ZM96,192H32V64H96a24,24,0,0,1,24,24V200A39.81,39.81,0,0,0,96,192Zm128,0H160a39.81,39.81,0,0,0-24,8V88a24,24,0,0,1,24-24h64ZM160,88h40a8,8,0,0,1,0,16H160a8,8,0,0,1,0-16Zm48,40a8,8,0,0,1-8,8H160a8,8,0,0,1,0-16h40A8,8,0,0,1,208,128Zm0,32a8,8,0,0,1-8,8H160a8,8,0,0,1,0-16h40A8,8,0,0,1,208,160Z',
+    backgroundImage: imageVisitantes.src
+  },
+  {
+    id: 'haeda',
+    href: '/haeda',
+    title: 'El cuento de Haeda',
+    description: 'Relato popular del Faedo para leer con niños',
+    icon: 'M184,112a8,8,0,0,1-8,8H112a8,8,0,0,1,0-16h64A8,8,0,0,1,184,112Zm-8,24H112a8,8,0,0,0,0,16h64a8,8,0,0,0,0-16Zm48-88V208a16,16,0,0,1-16,16H48a16,16,0,0,1-16-16V48A16,16,0,0,1,48,32H208A16,16,0,0,1,224,48ZM48,208H72V48H48Zm160,0V48H88V208H208Z',
+    backgroundImage: imageBruja.src
+  },
+  {
+    id: 'haya-fagus',
+    href: '/haya-fagus',
+    title: 'El Haya Fagus',
+    description: 'Árbol de 500 años, uno de los 100 más singulares de España',
+    icon: 'M198.1,62.59a76,76,0,0,0-140.2,0A71.71,71.71,0,0,0,16,127.8C15.9,166,48,199,86.14,200A72.09,72.09,0,0,0,120,192.47V232a8,8,0,0,0,16,0V192.47A72.17,72.17,0,0,0,168,200l1.82,0C208,199,240.11,166,240,127.8A71.71,71.71,0,0,0,198.1,62.59ZM169.45,184a56.08,56.08,0,0,1-33.45-10v-41l43.58-21.78a8,8,0,1,0-7.16-14.32L136,115.06V88a8,8,0,0,0-16,0v51.06L83.58,120.84a8,8,0,1,0-7.16,14.32L120,156.94v17a56,56,0,0,1-33.45,10C56.9,183.23,31.92,157.52,32,127.84A55.77,55.77,0,0,1,67.11,76a8,8,0,0,0,4.53-4.67,60,60,0,0,1,112.72,0A8,8,0,0,0,188.89,76,55.79,55.79,0,0,1,224,127.84C224.08,157.52,199.1,183.23,169.45,184Z',
+    backgroundImage: imageFagus.src
+  },
+  {
+    id: 'flora-fauna',
+    href: '/flora-fauna',
+    title: 'Flora y Fauna',
+    description: 'Biodiversidad completa del bosque y sus alrededores',
+    icon: 'M176,68a12,12,0,1,1-12-12A12,12,0,0,1,176,68Zm64,12a8,8,0,0,1-3.56,6.66L216,100.28V120A104.11,104.11,0,0,1,112,224H24a16,16,0,0,1-12.49-26l.1-.12L96,96.63V76.89C96,43.47,122.79,16.16,155.71,16H156a60,60,0,0,1,57.21,41.86l23.23,15.48A8,8,0,0,1,240,80Zm-22.42,0L201.9,69.54a8,8,0,0,1-3.31-4.64A44,44,0,0,0,156,32h-.22C131.64,32.12,112,52.25,112,76.89V99.52a8,8,0,0,1-1.85,5.13L24,208h26.9l70.94-85.12a8,8,0,1,1,12.29,10.24L71.75,208H112a88.1,88.1,0,0,0,88-88V96a8,8,0,0,1,3.56-6.66Z',
+    backgroundImage: imageNutria.src
+  },
+  {
+    id: 'geologia',
+    href: '/geologia',
+    title: 'Geología',
+    description: '400 millones de años de historia en las rocas',
+    icon: 'M246,98.73l-56-64A8,8,0,0,0,184,32H72a8,8,0,0,0-6,2.73l-56,64a8,8,0,0,0,.17,10.73l112,120a8,8,0,0,0,11.7,0l112-120A8,8,0,0,0,246,98.73ZM222.37,96H180L144,48h36.37ZM74.58,112l30.13,75.33L34.41,112Zm89.6,0L128,202.46,91.82,112ZM96,96l32-42.67L160,96Zm85.42,16h40.17l-70.3,75.33ZM75.63,48H112L76,96H33.63Z',
+    backgroundImage: imageGeologia.src
+  },
+  {
+    id: 'patrimonio-minero',
+    href: '/patrimonio-minero',
+    title: 'Patrimonio Minero',
+    description: 'Historia minera y el castillete de 1930',
+    icon: 'M224,152V136a96.37,96.37,0,0,0-64-90.51V40a16,16,0,0,0-16-16H112A16,16,0,0,0,96,40v5.49A96.37,96.37,0,0,0,32,136v16a16,16,0,0,0-16,16v24a16,16,0,0,0,16,16H224a16,16,0,0,0,16-16V168A16,16,0,0,0,224,152Zm-16-16v16H160V62.67A80.36,80.36,0,0,1,208,136ZM144,40V152H112V40ZM48,136A80.36,80.36,0,0,1,96,62.67V152H48Zm176,56H32V168H224v24Z',
+    backgroundImage: imageMineria.src
+  },
+  {
+    id: 'blog',
+    href: '/blog',
+    title: 'Blog',
+    description: 'Artículos y últimas novedades relacionadas con el Faedo de Ciñera',
+    icon: 'M216,40H40A16,16,0,0,0,24,56V216a8,8,0,0,0,11.58,7.15L64,208.94l28.42,14.21a8,8,0,0,0,7.16,0L128,208.94l28.42,14.21a8,8,0,0,0,7.16,0L192,208.94l28.42,14.21A8,8,0,0,0,232,216V56A16,16,0,0,0,216,40Zm0,163.06-20.42-10.22a8,8,0,0,0-7.16,0L160,207.06l-28.42-14.22a8,8,0,0,0-7.16,0L96,207.06,67.58,192.84a8,8,0,0,0-7.16,0L40,203.06V56H216ZM136,112a8,8,0,0,1,8-8h48a8,8,0,0,1,0,16H144A8,8,0,0,1,136,112Zm0,32a8,8,0,0,1,8-8h48a8,8,0,0,1,0,16H144A8,8,0,0,1,136,144ZM64,168h48a8,8,0,0,0,8-8V96a8,8,0,0,0-8-8H64a8,8,0,0,0-8,8v64A8,8,0,0,0,64,168Zm8-64h32v48H72Z',
+    backgroundImage: imageBlog.src
+  },
+
+
+];
+
+// Función helper para obtener cards por sus IDs
+export function getCardsByIds(ids: string[]): Card[] {
+  return ids
+    .map(id => allCards.find(card => card.id === id))
+    .filter((card): card is Card => card !== undefined);
+}
+
+// Función helper para obtener una card por su ID
+export function getCardById(id: string): Card | undefined {
+  return allCards.find(card => card.id === id);
+}
